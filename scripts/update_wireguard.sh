ec4795321158a55342ba25eacc99583e9f9dbfbf
Denis Efremov <efremov@linux.com>
2020-06-12 01:41:58 +0300

    scripts: add wireguard updater script
    Signed-off-by: Denis Efremov <efremov@linux.com>
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

---
+++ scripts/update_wireguard.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git clone git://git.zx2c4.com/wireguard-linux-compat
+rm -fr drivers/net/wireguard/*
+
+perl -i -ne 'BEGIN{$print=1;} $print = 0 if m/cat/; print $_ if $print;' wireguard-linux-compat/kernel-tree-scripts/create-patch.sh
+sed -i 's|net/wireguard|drivers/net/wireguard|g' wireguard-linux-compat/kernel-tree-scripts/create-patch.sh
+
+wireguard-linux-compat/kernel-tree-scripts/create-patch.sh | patch -p1
+
+rm -fr wireguard-linux-compat
