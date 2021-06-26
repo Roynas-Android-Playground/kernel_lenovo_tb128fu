8ebb5ba800c4885f80b13deea79ff7d84418d337
Chukun Pan <amadeus@jmu.edu.cn>
2021-06-26 11:20:08 +0800

    fs/ntfs3: compat: Backport support to kernel 4.19

--- fs/ntfs3/ntfs.h
+++ fs/ntfs3/ntfs.h
@@ -7,6 +7,15 @@
  * on-disk ntfs structs
  */
 
+// include/linux/compiler_attributes.h
+#ifndef fallthrough
+#if __has_attribute(__fallthrough__)
+#define fallthrough __attribute__((__fallthrough__))
+#else
+#define fallthrough do {} while (0)  /* fallthrough */
+#endif
+#endif
+
 /* TODO:
  * - Check 4K mft record and 512 bytes cluster
  */
