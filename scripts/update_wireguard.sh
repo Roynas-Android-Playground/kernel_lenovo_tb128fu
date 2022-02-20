abd9ecdb322412f1982fc40f8f86bdb9a02b2ac3
John Vincent <git@tenseventyseven.cf>
2022-02-20 12:04:03 +0800

    scripts: update_wireguard: Run `dos2unix` before patching
    On Windows development machines running WSL, we might encounter a 'bad interpreter' error as the script is cloned with DOS/Windows line endings which Bash doesn't like.
    
    Convert line endings to UNIX before running the script.
    
    Signed-off-by: John Vincent <git@tenseventyseven.cf>

--- scripts/update_wireguard.sh
+++ scripts/update_wireguard.sh
@@ -6,6 +6,8 @@ rm -fr drivers/net/wireguard/*
 perl -i -ne 'BEGIN{$print=1;} $print = 0 if m/cat/; print $_ if $print;' wireguard-linux-compat/kernel-tree-scripts/create-patch.sh
 sed -i 's|net/wireguard|drivers/net/wireguard|g' wireguard-linux-compat/kernel-tree-scripts/create-patch.sh
 
+dos2unix wireguard-linux-compat/kernel-tree-scripts/create-patch.sh
+
 wireguard-linux-compat/kernel-tree-scripts/create-patch.sh | patch -p1
 
 rm -fr wireguard-linux-compat
