bcdb9b443faeac179af2d73ed0377655fa17b8e3
roynatech2544 <whiteshell2544@naver.com>
2023-08-10 14:13:45 +0000

    Start of Grass Kernel

--- build_kernel.sh
+++ build_kernel.sh
@@ -9,5 +9,5 @@ export KBUILD_BUILD_HOST=GrassLand
 PATH=$PWD/toolchain/bin:$PATH
 
 rm -rf out
-make O=out ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 -j$(nproc) P89990GA1-perf_defconfig
+make O=out ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 -j$(nproc) grass-perf_defconfig
 make O=out ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 -j$(nproc)
