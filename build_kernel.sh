8ddf6bde9441c5a5205841afb1d728f1715206c7
roynatech2544 <whiteshell2544@naver.com>
2023-08-22 12:51:53 +0000

    build_kernel.sh: Auto regen defconfig on build

--- build_kernel.sh
+++ build_kernel.sh
@@ -12,4 +12,6 @@ git submodule update --init
 
 rm -rf out
 make O=out ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 -j$(nproc) grass-perf_defconfig
+make O=out ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 -j$(nproc) savedefconfig
+cp out/defconfig arch/arm64/configs/grass-perf_defconfig
 make O=out ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 -j$(nproc)
