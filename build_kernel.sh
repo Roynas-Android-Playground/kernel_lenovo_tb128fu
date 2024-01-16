4934ba8bfb13b4e94d4a232587a0da134a900ab5
Soo Hwan Na <whiteshell2544@naver.com>
2024-01-16 04:18:36 +0000

    ARM64: defconfig: grass: Disable CONFIG_AUDIT for script build

--- build_kernel.sh
+++ build_kernel.sh
@@ -11,7 +11,7 @@ PATH=$PWD/toolchain/bin:$PATH
 git submodule update --init
 
 rm -rf out
-make O=out ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 -j$(nproc) grass-perf_defconfig
+make O=out ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 -j$(nproc) grass-perf_defconfig noaudit.config
 make O=out ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 -j$(nproc) savedefconfig
 cp out/defconfig arch/arm64/configs/grass-perf_defconfig
 make O=out ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- LLVM=1 -j$(nproc)
