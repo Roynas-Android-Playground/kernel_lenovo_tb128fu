0e80fe305065821c16df964ccb4f6f2b19329404
Soo Hwan Na <whiteshell2544@naver.com>
2026-02-04 21:08:46 +0900

    Revert "extract-cert: Update from upstream"
    This reverts commit 3821312879c42a1e9bb6550a9b184fd4f147007b.

--- scripts/extract-cert.c
+++ scripts/extract-cert.c
@@ -21,17 +21,14 @@
 #include <openssl/bio.h>
 #include <openssl/pem.h>
 #include <openssl/err.h>
-#if OPENSSL_VERSION_MAJOR >= 3
-# define USE_PKCS11_PROVIDER
-# include <openssl/provider.h>
-# include <openssl/store.h>
-#else
-# if !defined(OPENSSL_NO_ENGINE) && !defined(OPENSSL_NO_DEPRECATED_3_0)
-#  define USE_PKCS11_ENGINE
-#  include <openssl/engine.h>
-# endif
-#endif
-#include "ssl-common.h"
+#include <openssl/engine.h>
+
+/*
+ * OpenSSL 3.0 deprecates the OpenSSL's ENGINE API.
+ *
+ * Remove this if/when that API is no longer used
+ */
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 
 #define PKEY_ID_PKCS7 2
 
@@ -39,14 +36,51 @@ static __attribute__((noreturn))
 void format(void)
 {
 	fprintf(stderr,
-		"Usage: extract-cert <source> <dest>\n");
+		"Usage: scripts/extract-cert <source> <dest>\n");
 	exit(2);
 }
+
+static void display_openssl_errors(int l)
+{
+	const char *file;
+	char buf[120];
+	int e, line;
+
+	if (ERR_peek_error() == 0)
+		return;
+	fprintf(stderr, "At main.c:%d:\n", l);
+
+	while ((e = ERR_get_error_line(&file, &line))) {
+		ERR_error_string(e, buf);
+		fprintf(stderr, "- SSL %s: %s:%d\n", buf, file, line);
+	}
+}
+
+#ifndef OPENSSL_IS_BORINGSSL
+static void drain_openssl_errors(void)
+{
+	const char *file;
+	int line;
+
+	if (ERR_peek_error() == 0)
+		return;
+	while (ERR_get_error_line(&file, &line)) {}
+}
+#endif
+
+#define ERR(cond, fmt, ...)				\
+	do {						\
+		bool __cond = (cond);			\
+		display_openssl_errors(__LINE__);	\
+		if (__cond) {				\
+			err(1, fmt, ## __VA_ARGS__);	\
+		}					\
+	} while(0)
 
 static const char *key_pass;
 static BIO *wb;
 static char *cert_dst;
-static bool verbose;
+int kbuild_verbose;
 
 static void write_cert(X509 *x509)
 {
@@ -58,82 +92,19 @@ static void write_cert(X509 *x509)
 	}
 	X509_NAME_oneline(X509_get_subject_name(x509), buf, sizeof(buf));
 	ERR(!i2d_X509_bio(wb, x509), "%s", cert_dst);
-	if (verbose)
+	if (kbuild_verbose)
 		fprintf(stderr, "Extracted cert: %s\n", buf);
-}
-
-static X509 *load_cert_pkcs11(const char *cert_src)
-{
-	X509 *cert = NULL;
-#ifdef USE_PKCS11_PROVIDER
-	OSSL_STORE_CTX *store;
-
-	if (!OSSL_PROVIDER_try_load(NULL, "pkcs11", true))
-		ERR(1, "OSSL_PROVIDER_try_load(pkcs11)");
-	if (!OSSL_PROVIDER_try_load(NULL, "default", true))
-		ERR(1, "OSSL_PROVIDER_try_load(default)");
-
-	store = OSSL_STORE_open(cert_src, NULL, NULL, NULL, NULL);
-	ERR(!store, "OSSL_STORE_open");
-
-	while (!OSSL_STORE_eof(store)) {
-		OSSL_STORE_INFO *info = OSSL_STORE_load(store);
-
-		if (!info) {
-			drain_openssl_errors(__LINE__, 0);
-			continue;
-		}
-		if (OSSL_STORE_INFO_get_type(info) == OSSL_STORE_INFO_CERT) {
-			cert = OSSL_STORE_INFO_get1_CERT(info);
-			ERR(!cert, "OSSL_STORE_INFO_get1_CERT");
-		}
-		OSSL_STORE_INFO_free(info);
-		if (cert)
-			break;
-	}
-	OSSL_STORE_close(store);
-#elif defined(USE_PKCS11_ENGINE)
-		ENGINE *e;
-		struct {
-			const char *cert_id;
-			X509 *cert;
-		} parms;
-
-		parms.cert_id = cert_src;
-		parms.cert = NULL;
-
-		ENGINE_load_builtin_engines();
-		drain_openssl_errors(__LINE__, 1);
-		e = ENGINE_by_id("pkcs11");
-		ERR(!e, "Load PKCS#11 ENGINE");
-		if (ENGINE_init(e))
-			drain_openssl_errors(__LINE__, 1);
-		else
-			ERR(1, "ENGINE_init");
-		if (key_pass)
-			ERR(!ENGINE_ctrl_cmd_string(e, "PIN", key_pass, 0), "Set PKCS#11 PIN");
-		ENGINE_ctrl_cmd(e, "LOAD_CERT_CTRL", 0, &parms, NULL, 1);
-		ERR(!parms.cert, "Get X.509 from PKCS#11");
-		cert = parms.cert;
-#else
-		fprintf(stderr, "no pkcs11 engine/provider available\n");
-		exit(1);
-#endif
-	return cert;
 }
 
 int main(int argc, char **argv)
 {
 	char *cert_src;
-	char *verbose_env;
 
 	OpenSSL_add_all_algorithms();
 	ERR_load_crypto_strings();
 	ERR_clear_error();
 
-	verbose_env = getenv("KBUILD_VERBOSE");
-	if (verbose_env && strchr(verbose_env, '1'))
-		verbose = true;
+	kbuild_verbose = atoi(getenv("KBUILD_VERBOSE")?:"0");
 
         key_pass = getenv("KBUILD_SIGN_PIN");
 
@@ -150,10 +121,33 @@ int main(int argc, char **argv)
 		fclose(f);
 		exit(0);
 	} else if (!strncmp(cert_src, "pkcs11:", 7)) {
-		X509 *cert = load_cert_pkcs11(cert_src);
+#ifdef OPENSSL_IS_BORINGSSL
+		ERR(1, "BoringSSL does not support extracting from PKCS#11");
+		exit(1);
+#else
+		ENGINE *e;
+		struct {
+			const char *cert_id;
+			X509 *cert;
+		} parms;
 
-		ERR(!cert, "load_cert_pkcs11 failed");
-		write_cert(cert);
+		parms.cert_id = cert_src;
+		parms.cert = NULL;
+
+		ENGINE_load_builtin_engines();
+		drain_openssl_errors();
+		e = ENGINE_by_id("pkcs11");
+		ERR(!e, "Load PKCS#11 ENGINE");
+		if (ENGINE_init(e))
+			drain_openssl_errors();
+		else
+			ERR(1, "ENGINE_init");
+		if (key_pass)
+			ERR(!ENGINE_ctrl_cmd_string(e, "PIN", key_pass, 0), "Set PKCS#11 PIN");
+		ENGINE_ctrl_cmd(e, "LOAD_CERT_CTRL", 0, &parms, NULL, 1);
+		ERR(!parms.cert, "Get X.509 from PKCS#11");
+		write_cert(parms.cert);
+#endif
 	} else {
 		BIO *b;
 		X509 *x509;
